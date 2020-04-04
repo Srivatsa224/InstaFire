07116f8d09238a3f1f47519f3af7617177414fda
srivatsa224 <srivatsa224@gmail.com>
2020-04-05 01:36:04 +0530

    Added Query to FireStore and created dummy userdata - Created Collections in Firebase console - Created Database in Firebase Console

--- app/src/main/java/com/srivatsa/instafire/PostsActivity.kt
+++ app/src/main/java/com/srivatsa/instafire/PostsActivity.kt
@@ -2,17 +2,44 @@ package com.srivatsa.instafire
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
 
-
-
+private const val TAG="PostsActivity"
 class PostsActivity : AppCompatActivity() {
+
+    //declaring inside onCreate, once we declare it here, it should not be null, so no ? at end of FirebaseFirestore
+
+    //make query to Firestore to retrieve data
+    private lateinit var firestoreDb: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_posts)
+
+        firestoreDb= FirebaseFirestore.getInstance()
+        val postsReference=firestoreDb.collection("posts")
+        //whenever there is any change in the db this gives us data
+        postsReference.addSnapshotListener { snapshot, exception ->
+            if(exception!==null || snapshot==null){
+                Log.e(TAG,"Exception when querying posts")
+                return@addSnapshotListener
+            }
+            for (document in snapshot.documents){
+                Log.i(TAG,"Document ${document.id}:${document.data}")
+            }
+        }
+
+
+
+
+
+
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
