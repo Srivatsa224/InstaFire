dc2c253c117185d8f4d3c921f0a9a3e913a72d35
srivatsa224 <srivatsa224@gmail.com>
2020-04-05 00:59:19 +0530

    Added Profile and Logout options

--- app/src/main/java/com/srivatsa/instafire/PostsActivity.kt
+++ app/src/main/java/com/srivatsa/instafire/PostsActivity.kt
@@ -1,12 +1,31 @@
 package com.srivatsa.instafire
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+
+
 
 class PostsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_posts)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+       menuInflater.inflate(R.menu.menu_posts,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId==R.id.menu_profile){
+            val intent=Intent(this,ProfileActivity::class.java)
+            startActivity(intent)
+
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
