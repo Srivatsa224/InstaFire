dc5a89c66f533b4bdc94381b3a0f162977f3ac18
srivatsa224 <srivatsa224@gmail.com>
2020-04-09 22:37:43 +0530

    Added ImageView

--- app/src/main/java/com/srivatsa/instafire/CreateActivity.kt
+++ app/src/main/java/com/srivatsa/instafire/CreateActivity.kt
@@ -1,12 +1,85 @@
 package com.srivatsa.instafire
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.srivatsa.instafire.models.User
+import kotlinx.android.synthetic.main.activity_create.*
+private const val TAG="CreateActivity"
+private const val PICK_PHOTO_CODE =123
 
 class CreateActivity : AppCompatActivity() {
+    private var photoUri: Uri?=null
+    private var signedInUser: User?=null
+    private lateinit var firestoreDb: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
+
+        firestoreDb= FirebaseFirestore.getInstance()
+
+        firestoreDb.collection("users")
+            .document(FirebaseAuth.getInstance().currentUser?.uid as String)
+            .get()
+            .addOnSuccessListener { userSnapshot ->
+                signedInUser=userSnapshot.toObject(User::class.java)
+                Log.i(TAG,"signed in user: $signedInUser")
+            }
+            .addOnFailureListener{ exception ->
+                Log.i(TAG,"Failed to fetch users", exception)
+            }
+
+        btnPickImage.setOnClickListener {
+            Log.i(TAG,"Open image picker on phone")
+            //implicit intent
+            val imagePickerIntent=Intent(Intent.ACTION_GET_CONTENT)
+            imagePickerIntent.type="image/*"
+            if(imagePickerIntent.resolveActivity(packageManager)!=null){
+                startActivityForResult(imagePickerIntent, PICK_PHOTO_CODE)
+            }
+        }
+        btnSubmit.setOnClickListener {
+            hadlesubmitbutton()
+        }
+
+
+    }
+
+    private fun hadlesubmitbutton() {
+        if(photoUri==null){
+            Toast.makeText(this,"Please select an image", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if(etDescription.text.isBlank()){
+            Toast.makeText(this,"Please enter the description",Toast.LENGTH_SHORT).show()
+            return
+        }
+        if(signedInUser==null){
+            Toast.makeText(this,"User not signed in, please wait", Toast.LENGTH_LONG).show()
+            return
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode== PICK_PHOTO_CODE){
+            if(resultCode==Activity.RESULT_OK){
+                 photoUri=data?.data
+                Log.i(TAG,"photoUri $photoUri")
+                imageView.setImageURI(photoUri)
+
+
+            }
+            else{
+                Toast.makeText(this,"Image picker action cancelled..",Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
