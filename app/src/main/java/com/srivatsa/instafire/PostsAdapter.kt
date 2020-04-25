c72658c66f386029a3dda50c2c19234435a0637e
srivatsa224 <srivatsa224@gmail.com>
2020-04-25 13:55:04 +0530

    Added profile pictures for users -Used Gravatar

--- app/src/main/java/com/srivatsa/instafire/PostsAdapter.kt
+++ app/src/main/java/com/srivatsa/instafire/PostsAdapter.kt
@@ -9,6 +9,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.srivatsa.instafire.models.Post
 import kotlinx.android.synthetic.main.item_post.view.*
+import java.math.BigInteger
+import java.security.MessageDigest
 
 class PostsAdapter (val context: Context, val posts:List<Post>):
     RecyclerView.Adapter<PostsAdapter.ViewHolder>()
@@ -29,11 +31,24 @@ class PostsAdapter (val context: Context, val posts:List<Post>):
     }
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(post: Post) {
+
+            val username=post.user?.username as String
             itemView.tvUsername.text=post.user?.username
             itemView.tvDescription.text=post.description
             Glide.with(context).load(post.imageUrl).into(itemView.ivPost)
+            Glide.with(context).load(getProfileImageUrl(username)).into(itemView.ivProfileImage)
             itemView.tvRelativeTime.text=DateUtils.getRelativeTimeSpanString(post.creationTimeMs)
 
+        }
+
+        //to generate profile picture
+
+        private fun getProfileImageUrl(username: String): String {
+            val digest = MessageDigest.getInstance("MD5")
+            val hash = digest.digest(username.toByteArray())
+            val bigInt = BigInteger(hash)
+            val hex = bigInt.abs().toString(16)
+            return "https://www.gravatar.com/avatar/$hex?d=identicon"
         }
     }
 }
